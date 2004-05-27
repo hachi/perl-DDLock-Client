661f1574ee304006fec2ae74e1812cd72b829c22
deveiant <deveiant@75bc8fc0-0210-0410-8e5e-a32055405bc5>
2004-05-27 00:44:32 +0000

    - Fleshed out into a first version.
    git-svn-id: http://code.sixapart.com/svn/ddlockd/trunk@8 75bc8fc0-0210-0410-8e5e-a32055405bc5

--- DDLockClient.pm
+++ DDLockClient.pm
@@ -1,36 +1,252 @@
 #!/usr/bin/perl
-#
+###########################################################################
 
+=head1 NAME
+
+DDLockClient - Client library for distributed lock daemon
+
+=head1 SYNOPSIS
+
+  use DDLockClient ();
+
+  my $cl = new DDLockClient (
+	servers => ['locks.localnet', 'locks2.localnet', 'localhost']
+  );
+
+  # Do something that requires locking
+  if ( my $lock = $cl->trylock("foo") ) {
+    ...do some 'foo'-synchronized stuff...
+  } else {
+    die "Failed to lock 'foo': $!";
+  }
+
+  # You can either just let $lock go out of scope or explicitly release it:
+  $lock->release;
+
+=head1 DESCRIPTION
+
+This is a client library for ddlockd, a distributed lock daemon not entirely
+unlike a very simplified version of the CPAN module IPC::Locker.
+
+=head1 REQUIRES
+
+L<Socket>
+
+=head1 EXPORTS
+
+Nothing.
+
+=head1 AUTHOR
+
+Brad Fitzpatrick <brad@danga.com>
+
+Copyright (c) 2004 Danga Interactive, Inc.
+
+=cut
+
+###########################################################################
+
+#####################################################################
+###	D D L O C K   C L A S S
+#####################################################################
+package DDLock;
+
+use Socket qw{:DEFAULT :crlf};
+use fields qw( name sockets );
+
+### (CONSTRUCTOR) METHOD: new( $name, @sockets )
+### Create a new lock object that corresponds to the specified I<name> and is
+### held by the given I<sockets>.
+sub new {
+    my DDLock $self = shift;
+    $self = fields::new( $self ) unless ref $self;
+
+    $self->{name} = shift;
+    $self->{sockets} = \@_;
+
+    return $self;
+}
+
+
+### METHOD: release()
+### Release the lock held by the lock object. Returns the number of sockets that
+### were released on success, and dies with an error on failure.
+sub release {
+    my DDLock $self = shift;
+
+    my (
+        $count,
+        $res,
+        $sock,
+       );
+
+    $count = 0;
+    while (( $sock = shift @{$self->{sockets}} )) {
+        $sock->print( "releaselock lock=$self->{name}$CRLF" );
+        chomp( $res = <$sock> );
+
+        unless ( $res =~ m{^ok\b}i ) {
+            my ($port, $iaddr) = sockaddr_in( getpeername($sock) );
+            my $addr = inet_ntoa( $iaddr );
+            die "releaselock ($addr): $res";
+        }
+
+        $count++;
+    }
+
+    return $count;
+}
+
+
+#####################################################################
+###	D D L O C K C L I E N T   C L A S S
+#####################################################################
 package DDLockClient;
 use strict;
 
-use fields qw(
-	      'servers',  # arrayref of lock servers
-	      );
+BEGIN {
+    use Socket qw{:DEFAULT :crlf};
+    use IO::Socket::INET ();
+    use fields qw( servers );
+    use vars qw{$Error};
+}
+
+$Error = undef;
+
+our $Debug = 0;
+
+
+### (CLASS) METHOD: DebugLevel( $level )
+sub DebugLevel {
+    my $class = shift;
+
+    if ( @_ ) {
+        $Debug = shift;
+        if ( $Debug ) {
+            *DebugMsg = *RealDebugMsg;
+        } else {
+            *DebugMsg = sub {};
+        }
+    }
+
+    return $Debug;
+}
+
+
+sub DebugMsg {}
+
+
+### (CLASS) METHOD: DebugMsg( $level, $format, @args )
+### Output a debugging messages formed sprintf-style with I<format> and I<args>
+### if I<level> is greater than or equal to the current debugging level.
+sub RealDebugMsg {
+    my ( $class, $level, $fmt, @args ) = @_;
+    return unless $Debug >= $level;
+
+    chomp $fmt;
+    printf STDERR ">>> $fmt\n", @args;
+}
+
+
+### (CONSTRUCTOR) METHOD: new( %args )
+### Create a new DDLockClient 
 sub new {
     my DDLockClient $self = shift;
     my %args = @_;
 
-    $self = fields::new($self) unless ref $self;    
-
-    die "Need 'servers' arrayref parameter to DDLockClient constructor" 
-	unless ref $args{servers} eq "ARRAY" && @$args{servers};
-
+    $self = fields::new( $self ) unless ref $self;
+    die "Need 'servers' arrayref parameter to DDLockClient constructor"
+        unless ref $args{servers} eq "ARRAY" && @{$args{servers}};
+    #$self->{servers} = $self->make_server_list( @{$args{servers}} );
     $self->{servers} = $args{servers};
+
     return $self;
 }
 
+
+### METHOD: trylock( $name )
+### Try to get a lock from the lock daemons with the specified I<name>. Returns
+### a DDLock object on success, and undef on failure.
 sub trylock {
     my DDLockClient $self = shift;
     my $lockname = shift;
 
-    
+    my (
+        @sockets,
+        $sock,
+        $res,
+       );
+
+    # First create connected sockets to all the lock hosts
+    @sockets = ();
+  SERVER: foreach my $server ( @{$self->{servers}} ) {
+        my ( $host, $port ) = split /:/, $server;
+        my $sock = new IO::Socket::INET (
+            PeerAddr    => $host,
+            PeerPort    => $port,
+            Proto       => "tcp",
+            Type        => SOCK_STREAM,
+            ReuseAddr   => 1,
+            Blocking    => 1,
+           ) or next SERVER;
+
+        $self->DebugMsg( 3, "Connected to %s:%d", $host, $port );
+        $self->DebugMsg( 5, "Sending command: trylock lock=$lockname" );
+        $sock->print( "trylock lock=$lockname$CRLF" );
+        chomp( $res = <$sock> );
+        $self->DebugMsg( 5, "Got response: $res" );
+        unless ( $res =~ m{^ok\b}i ) {
+            $self->DebugMsg( 3, "Failed lock: $res." );
+            return $self->lock_fail( "$server: '$lockname' $res" );
+        }
+
+        $self->DebugMsg( 2, "Adding $server to the list of lock hosts" );
+        push @sockets, $sock;
+    }
+
+    $self->DebugMsg( 1, "Got locks on %d hosts", scalar @sockets );
+    return $self->lock_fail( "No available lock hosts" ) unless @sockets;
+
+    # Now create a lock object to contain the connected sockets.
+    $self->DebugMsg( 2, "Returning new DDLock object." );
+    return DDLock->new( $lockname, @sockets );
 }
 
-package DDLock;
 
+### (PROTECTED) METHOD: lock_fail( $msg )
+### Set C<$!> to the specified message and return undef.
+sub lock_fail {
+    my DDLockClient $self = shift;
+    my $msg = shift;
+
+    $Error = $msg;
+    return undef;
+}
+
+
+### (PROTECTED) METHOD: make_server_list( @hosts )
+### Given a list of I<hosts> in the format "hostname:port", create and return an
+### array of sorted inetaddr records.
+sub make_server_list {
+    my DDLockClient $self = shift;
+    my @hosts = @_;
+
+    my @inetaddrs = ();
+    foreach my $host ( @hosts ) {
+        my ( $addr, $port ) = split /:/, $host;
+        push @inetaddrs, scalar sockaddr_in( $port, inet_aton($addr) );
+    }
+
+    return [ sort { $a cmp $b } @inetaddrs ];
+}
 
 
 
 
 1;
+
+# Local Variables:
+# mode: perl
+# c-basic-indent: 4
+# indent-tabs-mode: nil
+# End:
